5622df098faecf5497dce6b75e888ee5dd9b062c
James Hush <hello@jameshush.com>
2022-06-22 14:48:26 +0200

    Update domain

--- lib/mux-stack.ts
+++ lib/mux-stack.ts
@@ -1,4 +1,4 @@
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from "aws-cdk-lib";
 
 export class MuxStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -6,7 +6,7 @@ export class MuxStack extends cdk.Stack {
 
     const myDailyRecordingsBucket = new cdk.aws_s3.Bucket(
       this,
-      'MyDailyRecordings',
+      "MyDailyRecordings",
       {
         versioned: true,
         enforceSSL: true,
@@ -16,36 +16,36 @@ export class MuxStack extends cdk.Stack {
         // This will permenantly delete all files in the bucket
         // when the stack is deleted. This is helpful for testing a
         // proof of concept but will be disastorous in production.
-        removalPolicy: cdk.RemovalPolicy.DESTROY,
-        autoDeleteObjects: true,
+        // removalPolicy: cdk.RemovalPolicy.DESTROY,
+        // autoDeleteObjects: true,
       }
     );
 
-    const role = new cdk.aws_iam.Role(this, 'MuxExternal', {
+    const role = new cdk.aws_iam.Role(this, "MuxExternal", {
       description: "Role for Mux's external API",
       maxSessionDuration: cdk.Duration.hours(12),
-      assumedBy: new cdk.aws_iam.AccountPrincipal('291871421005'),
-      externalIds: ['hush'],
+      assumedBy: new cdk.aws_iam.AccountPrincipal("291871421005"),
+      externalIds: ["YOUR_DAILY_DOMAIN_HERE"],
     });
 
     role.addToPolicy(
       new cdk.aws_iam.PolicyStatement({
-        sid: 'VisualEditor0',
+        sid: "VisualEditor0",
         effect: cdk.aws_iam.Effect.ALLOW,
         actions: [
-          's3:PutObject',
-          's3:GetObject',
-          's3:ListBucketMultipartUploads',
-          's3:AbortMultipartUpload',
-          's3:ListBucketVersions',
-          's3:ListBucket',
-          's3:GetObjectVersion',
-          's3:ListMultipartUploadParts',
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucketMultipartUploads",
+          "s3:AbortMultipartUpload",
+          "s3:ListBucketVersions",
+          "s3:ListBucket",
+          "s3:GetObjectVersion",
+          "s3:ListMultipartUploadParts",
         ],
         // Connects the bucket to the role
         resources: [
           myDailyRecordingsBucket.bucketArn,
-          myDailyRecordingsBucket.arnForObjects('*'),
+          myDailyRecordingsBucket.arnForObjects("*"),
         ],
       })
     );
@@ -53,16 +53,16 @@ export class MuxStack extends cdk.Stack {
     // lambda to send a POST reques to mux
     const uploadToMuxLambdaFn = new cdk.aws_lambda_nodejs.NodejsFunction(
       this,
-      'uploadToMuxLambda',
+      "uploadToMuxLambda",
       {
         awsSdkConnectionReuse: true,
-        entry: './resources/upload-to-mux-lambda.ts',
+        entry: "./resources/upload-to-mux-lambda.ts",
         runtime: cdk.aws_lambda.Runtime.NODEJS_16_X,
         environment: {
-          MUX_TOKEN_ID: process.env.MUX_TOKEN_ID || '',
-          MUX_TOKEN_SECRET: process.env.MUX_TOKEN_SECRET || '',
+          MUX_TOKEN_ID: process.env.MUX_TOKEN_ID || "",
+          MUX_TOKEN_SECRET: process.env.MUX_TOKEN_SECRET || "",
         },
-        handler: 'handler',
+        handler: "handler",
         memorySize: 256,
         logRetention: cdk.aws_logs.RetentionDays.ONE_WEEK,
         tracing: cdk.aws_lambda.Tracing.ACTIVE,
